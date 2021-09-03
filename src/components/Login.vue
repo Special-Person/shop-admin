22d22750755376b34f4ccb9678fb41b35cb1b493
Special-Person <805321556@qq.com>
2021-09-03 21:08:35 +0800

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login_container">
+a
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        created() {
+            console.log(111);
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+.login_container{
+    background-color: #2b4b6b;
+}
+</style>
